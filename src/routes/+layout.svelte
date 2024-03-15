ea76d528dfb590b0dbc237b28f61cbe0dbc25cb9
Egaryan <hocok00@gmail.com>
2024-03-15 07:35:31 +0000

    second commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<nav>
+	<a href="/">home</a>
+	<a href="/about">about</a>
+</nav>
+
+<div class="layout">
+	<slot />
+</div>
+
+<style>
+	.layout{
+		background: rgb(136, 199, 85);
+        padding: 5px;
+	}
+</style> 
